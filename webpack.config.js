63b9f046ce16c1e31d1ba9485762d6b1c0386075
Javier Calzado <fjcalzadorodriguez@gmail.com>
2018-05-23 12:33:10 +0200

    Optimizing common chunks. Pointing to MUI ES6 index (now fixed in 1.0.0)

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,103 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 var basePath = __dirname;
 
 module.exports = {
-  context: path.join(basePath, 'src'),
+  context: path.join(basePath, "src"),
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: [".js", ".ts", ".tsx"],
+    alias: {
+      '@material-ui/core': "@material-ui/core/es",
+    }
   },
   entry: {
-    app: [
-      './app.tsx',
-      './style.scss',
-    ],
-    vendor: [
-      'react',
-      'react-dom',
-      '@material-ui/core',
-    ],
+    app: ["./app.tsx", "./style.scss"],
   },
   output: {
-    path: path.join(basePath, 'dist'),
-    filename: '[name].js',
+    path: path.join(basePath, "dist"),
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: 'awesome-typescript-loader',
+        loader: "awesome-typescript-loader",
         options: {
           useBabel: true,
         },
       },
       {
         test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ],
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader',
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
             options: {
               modules: true,
               camelCase: true,
               importLoaders: 1,
-              localIdentName: "[local]___[hash:base64:5]"
-            }},
-          { loader: 'sass-loader' },
+              localIdentName: "[local]___[hash:base64:5]",
+            },
+          },
+          { loader: "sass-loader" },
         ],
       },
       // Loading glyphicons => https://github.com/gowravshekar/bootstrap-webpack
       // Using here url-loader and file-loader
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
+        loader: "file-loader",
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
       },
     ],
   },
   // For development https://webpack.js.org/configuration/devtool/#for-development
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
     port: 8080,
     noInfo: true,
   },
-  mode: "development",
+  mode: process.env.NODE_ENV || "development",
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
   plugins: [
     //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html', //Name of file in ./dist/
-      template: 'index.html', //Name of template in ./src
+      filename: "index.html", //Name of file in ./dist/
+      template: "index.html", //Name of template in ./src
       hash: true,
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
+      analyzerMode: "static",
     }),
   ],
 };
